9a79bfcadd5cdfc35d9ef4179cac24c791116b7e
tratchapong <tratchapong@gmail.com>
2025-02-16 16:16:57 +0700

    mode ui App:PostContainer + PostItem

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CreatePost from './CreatePost'
+import PostItem from './PostItem'
 
 export default function PostContainer() {
   return (
     <>
       <div className="w-[680px] mx-auto min-h-screen my-3 flex flex-col gap-4 rounded-lg bg-red-100">
         <CreatePost />
+        <PostItem />
       </div>
     </>
   )
